cc7285f5edb95bd0bc68fa03bbf73c2bdd666e11
GarayJanica <janicagaray@122001@gmail.com>
2022-03-14 09:18:56 +0300

    My Educational Background added

--- git_python_garay.py
+++ git_python_garay.py
@@ -1,3 +1,7 @@
 print(Janica Ricamata Garay)
 print(Dawis Gasan Marinduque)
 print(November 12, 2001)
+
+print(Marinduque State College)
+print(Bachelor of Science in Information Technology)
+print(BSIT-2D)
